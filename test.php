5e850c638d0f7303060010ec38575b6ab33ec55a
prabhu183 <39380887+prabhu183@users.noreply.github.com>
2021-01-19 13:23:16 +0530

    Create test.php

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php 
+$rsel = array('first_name'=>'prabu','last_name'=>'ks','email_address'=>'prabhudil@gmail.com','contact_type'=>'RESELLER','contact_telephone'=>'');
+
+
+if(($r == $e) && (array_key_exists('first_name', $rsel)))
+{
+    $result = 'Ok';
+}
+else
+{
+    $result = 'Not Ok';
+}
+echo $result;
