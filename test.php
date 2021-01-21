83db565701686a7351b91d8aaa7b6d9e7555a3d9
prabhu183 <39380887+prabhu183@users.noreply.github.com>
2021-01-21 11:33:51 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,6 @@
 <?php 
-$rsel = array('first_name'=>'prabu','last_name'=>'ks','email_address'=>'prabhudil@gmail.com','contact_type'=>'RESELLER','contact_telephone'=>'');
+$rsel = array('first_name'=>'prabu','last_name'=>'ks','email_address'=>'prabhudil@gmail.com','contact_type'=>'RESELLER','contact_telephone'=>'',
+    "phone"=>'33123');
 
 
 if(($r == $e) && (array_key_exists('first_name', $rsel)))
