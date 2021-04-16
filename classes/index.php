69f8a4aa345dcc7e4848dab6286cf676fcecd068
prabhu197 <39380887+prabhu197@users.noreply.github.com>
2021-04-16 12:50:29 +0530

    Add files via upload

---
+++ classes/index.php
@@ -0,0 +1,17 @@
+<?php
+$r = 0;
+$e = 0;
+
+$rsel = array('first_name'=>'prabu','last_name'=>'ks','email_address'=>'prabhudil@gmail.com','contact_type'=>'RESELLER','contact_telephone'=>'',
+    "phone"=>'33123');
+
+
+if(($r == $e) && (array_key_exists('first_name', $rsel)))
+{
+    $result = 'Ok';
+}
+else
+{
+    $result = 'Not Ok';
+}
+echo $result;
